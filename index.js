a954e7d4a0f854e1bd8d2e3ccb24f164a40b5e35
Jeff Griffiths <jeff@eko-recordings.ca>
2019-11-10 06:24:08 -0800

    Added json file gathering, more test data, tests for json function.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const utils = require('./lib/utils');
+const fs = require('fs');
 
 const argv = require('yargs')
   .argv;
 
 if (require.main === module) {
   console.log(argv);
-  utils.getAllCSVSources(argv.root, (err, result) => {
-    if (err) throw err;
 
-    console.log(result);
-  });
+  if (argv.csvroot && fs.existsSync(argv.csvroot)) {
+    utils.getAllCSVSources(argv.csvroot, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
+
+  if (argv.jsonroot && fs.existsSync(argv.jsonroot)) {
+    utils.getAllJSONSources(argv.jsonroot, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+    });
+  }
 }
--- test/utils.test.js
+++ test/utils.test.js
@@ -1,9 +1,17 @@
 const utils = require('../lib/utils');
 
 test('we get some csv files', (done) => {
-  utils.getAllCSVSources('./test/test-data', (err, result) => {
+  utils.getAllCSVSources('./test/test-data/csv', (err, result) => {
     expect(result.length).toEqual(3);
     expect(result[0].endsWith('.csv')).toBeTruthy();
+    done();
+  });
+});
+
+test('we get some json files', (done) => {
+  utils.getAllJSONSources('./test/test-data/json', (err, result) => {
+    expect(result.length).toEqual(4);
+    expect(result[0].endsWith('.json')).toBeTruthy();
     done();
   });
 });
