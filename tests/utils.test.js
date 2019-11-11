bf9d304f3a92e43a78a3acd6f19b72aec8ee7e50
Jeff Griffiths <jeff@eko-recordings.ca>
2019-11-10 18:00:42 -0800

    folder re-name part 2

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,7 +1,7 @@
 const utils = require('../lib/utils');
 
 test('we get some csv files', (done) => {
-  utils.getAllCSVSources('./test/test-data/csv', (err, result) => {
+  utils.getAllCSVSources('./tests/test-data/csv', (err, result) => {
     expect(result.length).toEqual(3);
     expect(result[0].endsWith('.csv')).toBeTruthy();
     done();
@@ -9,7 +9,7 @@ test('we get some csv files', (done) => {
 });
 
 test('we get some json files', (done) => {
-  utils.getAllJSONSources('./test/test-data/json', (err, result) => {
+  utils.getAllJSONSources('./tests/test-data/json', (err, result) => {
     expect(result.length).toEqual(4);
     expect(result[0].endsWith('.json')).toBeTruthy();
     done();
